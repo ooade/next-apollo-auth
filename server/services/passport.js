b4660a266c45c07c0c0b1dd0d7a63e7393986be5
Ademola Adegbuyi <ooade96@gmail.com>
2019-01-21 20:45:02 +0100

    Fix #9: Also add prettier

--- server/services/passport.js
+++ server/services/passport.js
@@ -6,52 +6,52 @@ const User = require('../models/User')
 const GitHubStrategy = require('passport-github').Strategy
 
 passport.use(
-	new GitHubStrategy(
-		{
-			clientID: process.env.GithubClientID,
-			clientSecret: process.env.GithubClientSecret,
-			callbackURL: process.env.GithubCallbackURL,
-			passReqToCallback: true
-		},
-		(req, accessToken, refreshToken, profile, cb) => {
-			const { email, name, id } = profile._json
+  new GitHubStrategy(
+    {
+      clientID: process.env.GithubClientID,
+      clientSecret: process.env.GithubClientSecret,
+      callbackURL: process.env.GithubCallbackURL,
+      passReqToCallback: true
+    },
+    (req, accessToken, refreshToken, profile, cb) => {
+      const { email, name, id } = profile._json
 
-			// Check if user is auth'd
-			if (req.user) {
-				let user = req.user
+      // Check if user is auth'd
+      if (req.user) {
+        let user = req.user
 
-				user.github.id = id
-				user.github.email = email
-				user.github.name = name
+        user.github.id = id
+        user.github.email = email
+        user.github.name = name
 
-				user
-					.save()
-					.then(user => cb(null, user, { nextRoute: '/profile' }))
-					.catch(err => cb(err))
-			} else {
-				User.findOne({ 'github.id': id }).then(user => {
-					if (!user) {
-						// User is not auth, and not found on db? create an account
-						let newUser = new User()
+        user
+          .save()
+          .then(user => cb(null, user, { nextRoute: '/profile' }))
+          .catch(err => cb(err))
+      } else {
+        User.findOne({ $or: [{ 'github.id': id }, { email }] }).then(user => {
+          if (!user) {
+            // User is not auth, and not found on db? create an account
+            let newUser = new User()
 
-						newUser.email = email
-						newUser.github.id = id
-						newUser.github.email = email
-						newUser.github.name = name
+            newUser.email = email
+            newUser.github.id = id
+            newUser.github.email = email
+            newUser.github.name = name
 
-						newUser
-							.save()
-							.then(user => cb(null, user))
-							.catch(err => cb(err))
-					} else {
-						// user not auth'd but provider account found? log 'em in
-						// maybe update the profile here?
-						return cb(null, user)
-					}
-				})
-			}
-		}
-	)
+            newUser
+              .save()
+              .then(user => cb(null, user))
+              .catch(err => cb(err))
+          } else {
+            // user not auth'd but provider account found? log 'em in
+            // maybe update the profile here?
+            return cb(null, user)
+          }
+        })
+      }
+    }
+  )
 )
 
 passport.use(User.createStrategy())
